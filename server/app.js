4c209718719dc96299b8b9d04ba984620b524c02
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-01-18 16:01:12 +0100

    Update app.js
    Security improvements

--- server/app.js
+++ server/app.js
@@ -1,26 +1,58 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var markdown = require( "markdown" ).markdown;
+var markdown = require('markdown-it')({
+                                      html:         true,
+                                      xhtmlOut:     true,
+                                      breaks:       true,
+                                      langPrefix:   'language-',
+                                      linkify:      true,
+                                      typographer:  true,
+                                      quotes: '“”‘’',
+                                      highlight: function() {return '';}
+                                      });
 var fs = require('fs');
 app.get('/', function(req, res){
-        res.sendFile(__dirname + '/index.html');
+        res.sendFile(__dirname + '/chat.html');
         });
-var users = 0;
+var userArray = [];
 io.on('connection', function(socket){
-      users++;
-      io.emit('connections', users);
+      var currentUser = new Object();
+      socket.on('auth_details', function(username){
+                currentUser.username = username;
+                currentUser.index = userArray.length;
+                userArray.push(currentUser);
+                socket.emit('chat message', username + ' joined the chat');
+                });
+      socket.on('postName', function(username){
+                socket.emit('chat message', currentUser.username + ' changed his username to ' + username);
+                currentUser.username = username;
+                });
+      io.emit('connections', userArray);
       socket.on('chat message', function(msg){
-                io.emit('chat message', markdown.toHTML(msg));
+                var messageToBeSent = "<p>" + currentUser.username + ": " + markdown.renderInline(msg) + "</p>";
+                if(messageToBeSent.length <= 2048){
+                if(!verifyEmptyness(msg)){
+        io.emit('chat message', messageToBeSent);
+                }else{
+                    socket.emit('chat message', 'PM: Sorry, you cannot send empty messages.');
+                }
+                }else{
+                    socket.emit('chat message', 'PM: Oops! You cannot send messages longer than 2048 characters. Sorry!');
+                }
                 });
       socket.on('users', function(){
-                io.emit('connections', users);
+                
+                    io.emit('connections', userArray);
                 });
       socket.on('disconnect', function(){
-                users--;
-                io.emit('connections', users);
+                socket.emit('chat message', currentUser.username + ' left the chat');
+                io.emit('connections', userArray);
         });
       });
 http.listen(3000, function(){
             console.log('listening on *:3000');
             });
+var verifyEmptyness = function(str) {
+    return (str.length === 0 || !str.trim());
+};
