ff93475f315f25d9e6701a00250ad83816cf1041
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-01-22 20:59:43 +0100

    Update app.js

--- server/app.js
+++ server/app.js
@@ -16,53 +16,70 @@ app.get('/', function(req, res){
         });
 var userArray = [];
 io.on('connection', function(socket){
-      var currentUser = {};
       socket.on('auth_details', function(details){
-                currentUser.username = details.username;
-                currentUser.toString = function(){return this.name};
-                currentUser.location = userArray.length;
-                userArray.push(currentUser);
+                socket.username = details.username;
+                socket.toString = function(){return this.name};
                 });
       socket.on('postName', function(username){
-                currentUser.username = username;
+                socket.username = username;
                 });
-      io.emit('connections', userArray.join("<br>"));
+      var socketsConnected = socketConnections();
+      io.emit('connections', socketsConnected.length);
       socket.on('chat message', function(msg){
                 var escapedMessage = escapeHTML(msg.message);
                 var markedMessage =  markdown.renderInline(escapedMessage);
                 var messageToBeSent = "<p>" + escapeHTML(msg.username) + ": " + markedMessage + "</p>";
-                if(messageToBeSent.length <= 2048){
-                    if(!verifyEmptyness(msg.message)){
-                        io.emit('chat message', messageToBeSent);
-                    }else{
-                        socket.emit('chat message', 'PM: Sorry, you cannot send empty messages.');
-                    }
+                if(messageToBeSent.length <= 8192){
+                if(!verifyEmptyness(msg.message)){
+                io.emit('chat message', messageToBeSent);
                 }else{
-                    socket.emit('chat message', 'PM: Oops! You cannot send messages longer than 2048 characters. Sorry!');
+                socket.emit('chat message', 'PM: Sorry, you cannot send empty messages.');
+                }
+                }else{
+                socket.emit('chat message', 'PM: Oops! You cannot send messages longer than 8192 characters. Sorry!');
                 }
                 });
       socket.on('users', function(){
-                    io.emit('connections', userArray.join("<br>"));
+                var socketsConnected = socketConnections();
+                io.emit('connections', socketsConnected.length);
                 });
       socket.on('disconnect', function(){
-                userArray.splice(currentUser.location, 1);
-                io.emit('connections', userArray.join("<br>"));
-        });
+                var socketsConnected = socketConnections();
+                io.emit('connections', socketsConnected.length);
+                });
       });
-http.listen(3000, function(){
-            console.log('listening on *:3000');
+http.listen(3030, function(){
+            console.log('listening on *:3030');
             });
 var verifyEmptyness = function(str) {
     return (str.length === 0 || !str.trim());
 };
-function escapeHTML(text) {
-  var map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
-
-  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+function socketConnections(roomId, namespace) {
+    var res = []
+    , ns = io.of(namespace ||"/");    // the default namespace is "/"
+    
+    if (ns) {
+        for (var id in ns.connected) {
+            if(roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId) ;
+                if(index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            } else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
 }
+function escapeHTML(text) {
+    var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+    };
+    
+    return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+                          }
