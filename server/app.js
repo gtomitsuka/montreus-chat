d32763678e5b8394fb3442d206b63c839e3c85bd
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-01-25 11:16:02 +0100

    Added support to commands.

--- server/app.js
+++ server/app.js
@@ -17,29 +17,40 @@ app.get('/', function(req, res){
         });
 io.on('connection', function(socket){
       if(socketConnections().length <= 1024){
-      socket.on('auth_details', function(details){
-                socket.username = details.username;
-                socket.toString = function(){return this.name};
-                });
-      socket.on('postName', function(username){
+        socket.username = socket.handshake.query.username;
+        socket.toString = function(){return this.name};
+        socket.on('postName', function(username){
                 socket.username = username;
                 });
-      var socketsConnected = socketConnections();
-      io.emit('connections', socketsConnected.length);
-      socket.on('chat message', function(msg){
-                var markedMessage =  markdown.renderInline(msg.message);
-                var messageDate = moment(msg.date).format("LT, D/M");
-                var messageToBeSent = '<p class="alignLeft">' + escapeHTML(msg.username) + ': ' + markedMessage + '</p><p class="alignRight">' + messageDate + '</p>';
-                if(messageToBeSent.length <= 8192){
-                if(!verifyEmptyness(msg.message)){
-                io.emit('chat message', messageToBeSent);
+        var socketsConnected = socketConnections();
+        io.emit('connections', socketsConnected.length);
+        socket.on('chat message', function(msg){
+            var markedMessage =  markdown.renderInline(msg.message);
+            var messageDate = moment(msg.date).format("LT, D/M");
+            var firstWord = markedMessage.substr(0, markedMessage.indexOf(" "));
+            if(msg.message !== "/help"){
+            if(firstWord === "/broadcast"){
+            var finalMessage = markedMessage.substr(markedMessage.indexOf(" ") + 1);
+             var messageToBeSent = '<p class="alignLeft"> BROADCAST: ' + finalMessage + '</p><p class="alignRight">' + messageDate + '</p>';
+            }else if(firstWord === "/bot-say"){
+                var finalMessage = markedMessage.substr(markedMessage.indexOf(" ") + 1);
+                var messageToBeSent = '<p class="alignLeft"> Chat bot: ' + finalMessage + '</p><p class="alignRight">' + messageDate + '</p>';
+            }else{
+            var messageToBeSent = '<p class="alignLeft">' + escapeHTML(msg.username) + ': ' + markedMessage + '</p><p class="alignRight">' + messageDate + '</p>';
+            }
+            if(messageToBeSent.length <= 8192){
+                  if(!verifyEmptyness(msg.message)){
+                    io.emit('chat message', messageToBeSent);
+                  }else{
+                  socket.emit('chat message', 'PM: Sorry, you cannot send empty messages.');
+                  }
                 }else{
-                socket.emit('chat message', 'PM: Sorry, you cannot send empty messages.');
+                  socket.emit('chat message', 'PM: Oops! You cannot send messages longer than 8192 characters. Sorry!');
                 }
-                }else{
-                socket.emit('chat message', 'PM: Oops! You cannot send messages longer than 8192 characters. Sorry!');
-                }
-                });
+            }else{
+                socket.emit('chat message', 'Montreus Chat - v1.3.2<br>Available commands:<br>/help - Display help commands<br>/bot-say &lt;message&gt; - Give something for the bot to say!<br>/broadcast &lt;message&gt; - Broadcast a message');
+            }
+        });
       socket.on('users', function(){
                 var socketsConnected = socketConnections();
                 io.emit('connections', socketsConnected.length);
