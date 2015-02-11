2b379f0be96aed90d10884ad38ff9ea7ad77a34f
Gabriel Tomitsuka <gabriel.tomitsuka@montreus.com>
2015-02-11 19:47:10 +0100

    Update db.js

--- server/db.js
+++ server/db.js
@@ -5,3 +5,27 @@
  //APIs
  var mongoose = require("mongoose");
  
+//Globals
+var Schema = mongoose.Schema;
+var ObjectId = mongoose.Types.ObjectId; 
+
+//Startup Routines
+mongoose.connect('mongodb://localhost/chat-db', function (error) {
+  if (error) {
+      console.warn(error);
+    }else{
+      console.log("Connection to MongoDB established");
+    }
+});
+
+//MongoDB Globals
+var MessageSchema = new Schema({
+    message: String,
+    room: String,
+    sent: Date
+});
+var Message = mongoose.model('Message', UserSchema);
+//
+var findMessages = function(room) {
+    Message.find({}).sort({date: -1}).exec(function(err, docs) {});
+}
